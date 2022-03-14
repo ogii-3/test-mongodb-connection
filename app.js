d9d9473b282d4e3dfb15bc298ff2313cd4ddbe24
ogii-3 <81627882+ogii-3@users.noreply.github.com>
2022-03-14 15:25:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const {MongoClient} = require('mongodb');
-const mongoDB = 'mongodb://mongodb:27017/';
-const url = 'mongodb://localhost:27017/test'
+#const {MongoClient, Logger} = require('mongodb');
+#const mongoDB = 'mongodb://mongodb:27017/';
+const uri = 'mongodb://localhost:27017/test'
 
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+//MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function main(client) {
+  // Set debug level
+  Logger.setLevel("debug");
+  const db = client.db("sample_mflix");
+  // Run a sample command to produce logger messages
+  await db.command({ isMaster: true });
+}
