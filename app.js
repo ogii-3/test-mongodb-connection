3d8d208df6949665979443c0eb431c78f070d7fe
ogii-3 <81627882+ogii-3@users.noreply.github.com>
2022-03-14 15:42:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-const {MongoClient, Logger} = require('mongodb');
-//#const mongoDB = 'mongodb://mongodb:27017/';
+const {MongoClient} = require('mongodb');
 const uri = 'mongodb://localhost:27017/test'
-
-//MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-/*const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});*/
-
-/*async function main(client) {
-  // Set debug level
-  Logger.setLevel("debug");
-  const db = client.db("sample_mflix");
-  // Run a sample command to produce logger messages
-  await db.command({ isMaster: true });
-}*/
 
 MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
   if (err) throw err;
@@ -24,12 +8,6 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (e
   dbo.createCollection("testcollection", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
-    //db.close();
-  });
-  
-  dbo.collection("testcollection").find().toArray(function(err, res) {
-    if (err) throw err;
-    console.log(res);
     db.close();
-  })
+  });
 })
